588085e35d4b97fc92909a0dd3df3f31d887c95a
juarezfrench <markedfrench@gmail.com>
2019-08-27 17:45:01 -0500

    Spacing tweaks.

--- safe-routes/src/App.js
+++ safe-routes/src/App.js
@@ -11,26 +11,24 @@ import {Button, Image, Input} from 'semantic-ui-react';
 
 const App = () => {
   return (
-   
 
-  
-  
+    // ***************UI Elements ***************
+
   <section className="container">  
 
    <h1>Safe Routes</h1>
    <Image src={SampleMap} 
       alt="googleMap" className= "main-image" zindex= "-1" />
 
-
   <section className='contents'> 
     <div className="button-container">
      <Input className="search-box" icon='search' placeholder='Search...' />    
       <Button primary>Login</Button>
     </div>
-  
-
-          
+            
     </section>
+ {/* // ***************Routing *************** */}
+
 <section className="routing">
 {/* <Switch>
 <Route path="/" exact component={App} />
