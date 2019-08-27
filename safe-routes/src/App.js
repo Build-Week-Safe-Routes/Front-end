97ea474fbf7b92b077662e542879fe5c0e524525
juarezfrench <markedfrench@gmail.com>
2019-08-27 10:19:47 -0500

    Fist commit - scaffolding app

--- safe-routes/src/App.js
+++ safe-routes/src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import './App.css';
+import SampleMap from "./assets/SampleMap.png"
+import ResizeImage from 'react-resize-image'
+import {Button, Dropdown, Icon} from 'semantic-ui-react'
+
+
 
 const App = () => {
+
+ 
+  
   return (
-    <div className="App">
+    
+    <section>    
+      <div class="login-button">
+         <Button primary>Login</Button>
     </div>
-  );
+  
+    
+  <Dropdown
+    text='Filter'
+    icon='filter'
+    floating
+    labeled
+    button
+    className='icon'
+  >
+    <Dropdown.Menu>
+      <Dropdown.Header icon='tags' content='Filter by tag' />
+      <Dropdown.Divider />
+      <Dropdown.Item>
+        <Icon name='wrench' className='right floated' />
+        Important
+      </Dropdown.Item>
+      <Dropdown.Item>
+        <Icon name='wrench' className='right floated' />
+        Announcement
+      </Dropdown.Item>
+      <Dropdown.Item>
+        <Icon name='conversation' className='right floated' />
+        Discussion
+      </Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+   
+    <ResizeImage src= {SampleMap} alt="googleMap"
+      width="100%" height="100%" gravity="faces" crop="fill" />
+          
+    </section>
+
+  )
 }
 
 export default App;
